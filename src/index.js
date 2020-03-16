9fd96d4c4c183315b61bcc670d1239ee94dea174
Kate <braumwinter@gmail.com>
2020-03-16 14:36:23 +0300

    feat: add solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0){
+        n = -n;
+    }
+    let str_temp = String(n);
+    let arr = str_temp.split('').reverse();
+
+    if(arr[0] === 0){
+        arr.shift();
+    }
+
+    let str = arr.join('');
+
+    return Number(str);
 }
